8fb8641c4432483e88257d17417769bb9c63c448
Edwy Lugo <edwylugo@gmail.com>
2020-07-13 16:51:45 -0300

    - Animando o Loading

--- Tinder/View/Loading.swift
+++ Tinder/View/Loading.swift
@@ -20,10 +20,26 @@ class Loading: UIView {
         return load
     }()
     
+    let perfilImageView: UIImageView = {
+        let imagem = UIImageView()
+        imagem.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        imagem.layer.cornerRadius = 50
+        imagem.layer.borderWidth = 5
+        imagem.layer.borderColor = UIColor.white.cgColor
+        imagem.clipsToBounds = true
+        imagem.image = UIImage(named: "perfil")
+        return imagem
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame:frame)
         addSubview(loadView)
         loadView.center = center
+        
+        addSubview(perfilImageView)
+        perfilImageView.center = center
+        
+        
         self.animacao()
     }
     
@@ -36,8 +52,13 @@ class Loading: UIView {
             self.loadView.frame = CGRect(x: 0, y: 0, width: 250, height: 250)
             self.loadView.center = self.center
             self.loadView.layer.cornerRadius = 125
+            self.loadView.alpha = 0.3
         }) {(_) in
-            
+            self.loadView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+            self.loadView.center = self.center
+            self.loadView.layer.cornerRadius = 50
+            self.loadView.alpha = 1
+            self.animacao()
         }
     }
     
