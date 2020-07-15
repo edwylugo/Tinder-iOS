542df87727d7a22caeac2a950af9ebf2eb1edbd9
Edwy Lugo <edwylugo@gmail.com>
2020-07-15 15:36:26 -0300

    - Efeito Elastico

--- Tinder/Controller/DetalheVC.swift
+++ Tinder/Controller/DetalheVC.swift
@@ -7,13 +7,39 @@
 //
 
 import UIKit
+
+class HeaderLayout: UICollectionViewFlowLayout {
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        let layoutAttributes = super.layoutAttributesForElements(in: rect)
+        
+        layoutAttributes?.forEach({ (attribute) in
+            if attribute.representedElementKind == UICollectionView.elementKindSectionHeader {
+                guard let collectionView = collectionView else {return}
+                let contentOffSetY = collectionView.contentOffset.y
+                attribute.frame = CGRect(
+                    x: 0,
+                    y: contentOffSetY,
+                    width: collectionView.bounds.width,
+                    height: attribute.bounds.height - contentOffSetY)
+            }
+        })
+        
+        return layoutAttributes
+    }
+    
+    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        return true
+    }
+    
+}
+
 class DetalheVC: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     let cellId = "cellId"
     let headerId = "headerId"
     
     init() {
-        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+        super.init(collectionViewLayout: HeaderLayout())
     }
     
     required init?(coder: NSCoder) {
